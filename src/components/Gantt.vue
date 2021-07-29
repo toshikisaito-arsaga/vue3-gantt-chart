02dcfcb05cb8091736906b9b5b0f508256b80178
ToshikiSaito <toshiki_s@arsaga.jp>
2021-07-29 20:29:28 +0900

    feat: implement input form feature

--- src/components/Gantt.vue
+++ src/components/Gantt.vue
@@ -28,7 +28,90 @@
         <div class="base" v-show="show">
           <div class="overlay" v-show="show" @click="show = false"></div>
           <div class="content" v-show="show">
-            フォーム
+            <h2 class="font-bold">タスクの追加</h2>
+            <div class="my-4">
+              <label class="text-xs">カテゴリーID:</label>
+              <select
+                v-model="form.category_id"
+                class="text-xs border px-4 py-2 rounded-lg"
+              >
+                <option
+                  v-for="category in categories"
+                  :key="category.id"
+                  :value="category.id"
+                  >{{ category.name }}
+                </option>
+              </select>
+            </div>
+            <div class="my-4">
+              <label class="text-xs">ID:</label>
+              <input
+                class="text-xs border rounded-lg px-4 py-2"
+                v-model.number="form.id"
+              />
+            </div>
+            <div class="my-4">
+              <label class="text-xs">タスク名:</label>
+              <input
+                class="text-xs border rounded-lg px-4 py-2"
+                v-model="form.name"
+              />
+            </div>
+            <div class="my-4">
+              <label class="text-xs">担当者:</label>
+              <input
+                class="text-xs border rounded-lg px-4 py-2"
+                v-model="form.incharge_user"
+              />
+            </div>
+            <div class="my-4">
+              <label class="text-xs">開始日:</label>
+              <input
+                class="text-xs border rounded-lg px-4 py-2"
+                v-model="form.start_date"
+                type="date"
+              />
+            </div>
+            <div class="my-4">
+              <label class="text-xs">完了期限日:</label>
+              <input
+                class="text-xs border rounded-lg px-4 py-2"
+                v-model="form.end_date"
+                type="date"
+              />
+            </div>
+            <div class="my-4">
+              <label class="text-xs">進捗度:</label>
+              <input
+                class="text-xs border rounded-lg px-4 py-2"
+                v-model="form.percentage"
+                type="number"
+              />
+            </div>
+            <div>
+              <button
+                @click="saveTask"
+                class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-lg flex items-center"
+              >
+                <svg
+                  class="w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                  />
+                </svg>
+                <span class="font-bold text-xs">
+                  タスクを追加する
+                </span>
+              </button>
+            </div>
           </div>
         </div>
       </teleport>
@@ -413,6 +496,15 @@ export default {
       ],
       task: "",
       show: false,
+      form: {
+        category_id: "",
+        id: "",
+        name: "",
+        start_date: "",
+        end_date: "",
+        incharge_user: "",
+        percentage: 0,
+      },
     };
   },
   methods: {
@@ -608,6 +700,11 @@ export default {
     },
     addTask() {
       this.show = true;
+    },
+    saveTask() {
+      this.tasks.push(this.form);
+      this.form = {};
+      this.show = false;
     },
   },
   computed: {
