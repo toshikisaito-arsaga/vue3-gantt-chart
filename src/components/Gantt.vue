14945e8e38a3a5cd18e461a106f131763e7c7c4a
ToshikiSaito <toshiki_s@arsaga.jp>
2021-07-29 20:44:44 +0900

    feat: implement delet task feature

--- src/components/Gantt.vue
+++ src/components/Gantt.vue
@@ -110,6 +110,26 @@
                 </svg>
                 <span class="text-xs font-bold text-white">タスクを更新</span>
               </button>
+              <button
+                @click="deleteTask(form.id)"
+                class="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded-lg flex items-center ml-2"
+              >
+                <svg
+                  class="w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                  />
+                </svg>
+                <span class="text-xs font-bold text-white">タスクを削除</span>
+              </button>
             </div>
             <div v-else>
               <button
@@ -741,6 +761,15 @@ export default {
     updateTask(id) {
       let task = this.tasks.find((task) => task.id === id);
       Object.assign(task, this.form);
+      this.form = {};
+      this.show = false;
+    },
+    deleteTask(id) {
+      let delete_index;
+      this.tasks.map((task, index) => {
+        if (task.id === id) delete_index = index;
+      });
+      this.tasks.splice(delete_index, 1);
       this.form = {};
       this.show = false;
     },
