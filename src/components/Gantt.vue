a185eeeff2b8c5936f276ec516517780903100c8
ToshikiSaito <toshiki_s@arsaga.jp>
2021-07-29 19:56:24 +0900

    feat: implement add task button

--- src/components/Gantt.vue
+++ src/components/Gantt.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
     <div id="gantt-header" class="h-12 p-2 flex items-center">
-      <h1 class="text-xl font-bold">ガントチャート</h1>
+      <h1 class="text-sm font-bold">ガントチャート</h1>
+      <button
+        @click="addTask"
+        class="bg-indigo-700 hover:bg-indigo-900 text-white py-2 px-4 rounded-lg flex items-center"
+      >
+        <svg
+          class="w-4"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+        <span class="font-bold text-xs">
+          タスクを追加する
+        </span>
+      </button>
     </div>
     <div id="gantt-content" class="flex">
       <div id="gantt-task">
@@ -574,6 +596,9 @@ export default {
         (category) => category.id === task_id
       );
       category["collapsed"] = !category["collapsed"];
+    },
+    addTask() {
+      console.log("click");
     },
   },
   computed: {
